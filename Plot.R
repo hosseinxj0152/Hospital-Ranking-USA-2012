1a2e9ee4fa3f5f6bacb0fb521a782a869abda5ec
hosseinxj0152 <hosseinxj0152Hayabusa@gmail.com>
2021-02-14 02:42:08 +0330

    Delete Plot.R

--- Plot.R
+++
@@ -1,6 +0,0 @@
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-head(outcome)
-outcome[,11] <- as.numeric(outcome[,11])
-hist(outcome[,11])
-names(outcome)
